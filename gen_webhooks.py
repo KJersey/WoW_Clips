8d721d54e872cc894a061a041094b560712a6c2b
Jersey <k.aubindery@gmail.com>
2023-04-24 23:05:08 -0400

    Removed script.py

--- gen_webhooks.py
+++ gen_webhooks.py
@@ -3,19 +3,21 @@ import sys
 
 def main():
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print("Invalid number of arguments. Please run the program with 'python3 gen_webhooks.py webhooks.json'")
+        return
 
-    webhooks_file = open("webhooks.json")
-    webhooks_data = json.load(webhooks_file)
+    webhooks_file_path = sys.argv[1]
+    with open(webhooks_file_path) as webhooks_file:
+        webhooks_data = json.load(webhooks_file)
 
-    discohook_file = open("discohook.json")
-    discohook_data = json.load(discohook_file)
+    with open("discohook.json") as discohook_file:
+        discohook_data = json.load(discohook_file)
 
     for boss in webhooks_data:
         webhook = webhooks_data[boss]['webhook']
         thread_id = webhooks_data[boss]['thread_id']
-        discohook = discohook_data[boss]['discohook']
+        discohook = discohook_data[boss]
         print(f'{boss}\n{discohook}\n{webhook}?thread_id={thread_id}\n')
 
 if __name__ == "__main__":
